6db9a5a069719dfe0b42f3e3bf420aa41151262a
Adam Gospodarczyk <adam@overment.com>
2022-12-02 01:12:02 +0100

    Setup Deployment

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,27 @@
+export default {
+    base: '/',
+    title: 'brain.overment.com',
+    description: 'Everything I know',
+    head: [],
+    themeConfig: {
+        logo: '/assets/img/logo-symbol.svg',
+        sidebar: [
+            {
+                text: 'Start',
+                items: [
+                    { text: 'Start here', link: '/' }
+                ]
+            },
+            {
+                text: 'Core',
+                items: [
+                    { text: 'Direction', link: '/Core/Direction.md' },
+                    { text: 'Learning System', link: '/Core/Learning System.md' },
+                    { text: 'Process', link: '/Core/Process.md' },
+                    { text: 'Knowing thyself', link: '/Core/Knowing thyself.md' },
+                    { text: 'Values', link: '/Core/Values.md' },
+                ]
+            },
+        ]
+    }
+}
